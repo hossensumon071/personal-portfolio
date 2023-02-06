a8f15b6b06a043431cb031a439a2ebe7a9fa2b5d
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-02-06 15:01:06 +0600

    navbar area done

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Bio = () => {
   return (
-    <div>
-      
+    <div className="bio">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sunt sapiente labore unde vero excepturi porro explicabo, sequi eaque itaque voluptatibus fugiat iste deleniti inventore reiciendis commodi temporibus velit minus molestias expedita doloribus quae a! Repellat molestias, tenetur est laborum porro laboriosam, veniam debitis eum cum vitae accusamus ratione optio.</p>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className='flex justify-between container mx-auto mt-20 uppercase'>
+      <div>
+        <HashLink smooth to="#home" className='link-item'>
+            Amir Khan Sumon
+        </HashLink>
+      </div>
+      <ul className="links flex flex-col gap-3">
+        <li><HashLink smooth to="#home" className='link-item'>Home</HashLink></li>
+        <li><HashLink smooth to="#home" className='link-item'>Projects</HashLink></li>
+        <li><HashLink smooth to="#home" className='link-item'>Skills</HashLink></li>
+        <li><HashLink smooth to="#home" className='link-item'>About</HashLink></li>
+        <li><HashLink smooth to="#home" className='link-item'>Contact</HashLink></li>
+        <li><a 
+        href="www.google.com"
+        target="_blank"
+        rel="noreferrer"
+        className='link-item'
+        >
+          Resume
+        </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
